25d8f80430c9d6739b5af6ecfeb204f0cec95862
camifernandez2003 <camila.fernandez@ucuenca.edu.ec>
2024-10-07 11:03:39 -0500

    Se hicieron ajustes al código

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ let video;
 let flippedVideo;
 // To store the classification
 let label = "";
+let confianza = 0;
 
 // Load the model first
 function preload() {
@@ -21,7 +22,7 @@ function setup() {
   video.size(320, 240);
   video.hide();
 
-  flippedVideo = ml5.flipImage(video);
+  //flippedVideo = ml5.flipImage(video);
   // Start classifying
   classifyVideo();
 }
@@ -29,24 +30,28 @@ function setup() {
 function draw() {
   background(0);
   // Draw the video
-  image(flippedVideo, 0, 0);
+  image(video, 0, 0);
 
   // Draw the label
   fill(255);
   textSize(16);
   textAlign(CENTER);
   text(label, width / 2, height - 4);
+
+  textSize(8);
+  textAlign(LEFT);
+  text(confianza, 10, height - 4);
 }
 
 // Get a prediction for the current video frame
 function classifyVideo() {
-  flippedVideo = ml5.flipImage(video);
-  classifier.classify(flippedVideo, gotResult);
-  flippedVideo.remove();
+  //flippedVideo = ml5.flipImage(video);
+  classifier.classify(video, gotResult);
+  //flippedVideo.remove();
 }
 
 // When we get a result
-function gotResult(error, results) {
+function gotResult(results, error) {
   // If there is an error
   if (error) {
     console.error(error);
@@ -55,6 +60,7 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
+  confianza = results[0];
   // Classifiy again!
   classifyVideo();
 }
